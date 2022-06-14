f63de345ed3a23ea2f5f332bef4bb84e5afa6008
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2022-06-14 17:40:04 +0300

    small style changes

---
+++ src/components/Products.tsx
@@ -0,0 +1,15 @@
+import Layout from "./layouts/Layout";
+
+function Products() {
+  return (
+    <>
+      <Layout>
+        <p>Suris</p>
+        <p>Mesa</p>
+        <p>Duona</p>
+      </Layout>
+    </>
+  );
+}
+
+export default Products;
