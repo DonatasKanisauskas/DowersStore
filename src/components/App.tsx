824483acc7c669ad2cc2dd735e587e0764905f0b
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2022-06-14 17:39:38 +0300

    changed jsx to tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import logo from '../logo.svg';
+import '../styles/App.sass';
+import Products from './Products';
+
+function App() {
+	return (
+		// <div className="App">
+		//   <header className="App-header">
+		//     <img src={logo} className="App-logo" alt="logo" />
+		//     <p>
+		//       React Shop
+		//     </p>
+		//   </header>
+		// </div>
+		<Products />
+	);
+}
+
+export default App;
