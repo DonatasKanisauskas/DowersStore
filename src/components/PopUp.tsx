e3a42db16ec1ba307361be34eb4cb21940378e92
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2022-06-17 19:57:27 +0300

    Added popup window

---
+++ src/components/PopUp.tsx
@@ -0,0 +1,19 @@
+import logo from '../logo.svg';
+import '../styles/Products.sass';
+
+
+function PopUp() {
+  return (
+    <div className='product_card'>
+      <div className='product_image'>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="product_container">
+        <h4><b>Dvaro suris</b></h4>
+        <p>Fermentinis suris</p>
+      </div>
+    </div>
+  );
+}
+
+export default PopUp;
