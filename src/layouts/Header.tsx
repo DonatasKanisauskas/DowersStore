feb354b697c9d402bedc8b84aa43c28f9f157f0d
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2022-07-22 23:11:57 +0300

    navbar phone dropdown handling and styling

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 
 function Header() {
   const [showHeader, setShowHeader] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
+
+  const handleDropDown = () => {
+    let ele: HTMLElement | null = document.querySelector("div .navbar_dropdown");
+    if (ele) {
+      ele.style.maxHeight = (dropDown) ? "0" : "500px";
+      setDropDown(!dropDown);
+    }
+  }
 
   var prevScrollpos = window.pageYOffset;
   window.onscroll = function () {
@@ -12,20 +21,25 @@ function Header() {
     } else {
       setShowHeader(true);
     }
+    if(dropDown){
+      handleDropDown();
+    }
     prevScrollpos = currentScrollPos;
   }
 
   return (
-    <div className="header flex" style={{ top: showHeader ? '-50px' : '0' }} >
-      <Link className="navbar_button title inline noShrink" to="/">React Shop</Link>
-      {/* button for phone dropdown menu */}
-      <div className="flex grow space-between">
-        <div className="inline">
+    <div className="navbar flex" style={{ top: showHeader ? '-50px' : '0' }} >
+      <div className="navbar_mobile">
+        <Link className="navbar_button title inline noShrink" to="/">React Shop</Link>
+        <button className="navbar_button_dropdown" onClick={handleDropDown}>=</button>
+      </div>
+      <div className="navbar_dropdown flex grow space-between">
+        <div className="navbar_dropdown_section inline">
           <Link className="navbar_button inline" to="#link1">Link1</Link>
           <Link className="navbar_button inline" to="#link2">Link2</Link>
           <Link className="navbar_button inline" to="#link3">Link3</Link>
         </div>
-        <div className="inline">
+        <div className="navbar_dropdown_section inline">
           <Link className="navbar_button inline" to="#user">User</Link>
           <Link className="navbar_button inline" to="/cart">Cart</Link>
         </div>
