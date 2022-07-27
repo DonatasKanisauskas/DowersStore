52469ecbf1395b2d02f09fefdbaa5eb1e9e6a6a6
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2022-07-28 00:57:26 +0300

    displaying categories

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,21 +1,23 @@
 import '../assets/styles/Products.sass';
 import Product, { productType } from './Product';
 import { useState, useEffect } from "react";
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import PopUp from './PopUp';
 
 
 function Products() {
+  const { category } = useParams();
+  const { state } = useLocation();
   const [products, setProducts] = useState<productType>();
+  const [categories, setCategories] = useState<Array<String>>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<any>(null);
-  const { state } = useLocation();
 
   const fetchProducts = async () => {
     setLoading(true);
     try {
       const response = await fetch(
-        `https://dummyjson.com/products`
+        `https://dummyjson.com/products${category ? "/category/" + category : ""}`
       );
       const data = await response.json();
       setProducts(data.products);
@@ -28,26 +30,50 @@ function Products() {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/products/categories`
+      );
+      const data = await response.json();
+      setCategories(data);
+    }
+    catch (err: any) {
+      setError(err.message);
+    }
+  };
+
   const closePopup = () => {
     window.history.replaceState({}, document.title);
     setError(null);
   }
 
   useEffect(() => {
+    console.log("test");
     fetchProducts();
     setError(state);
-  }, []);
+    fetchCategories();
+  }, [category]);
 
   return (
     <>
       <h1>Products</h1>
-      {loading &&
-        <>Loading data...</>
-      }
       {error &&
         <PopUp closePopup={closePopup} error={error} />
       }
+      <div className="categories flex wrap center">
+        <h4 className='noShrink'>Categories</h4>
+        <a href="/products">All</a>
+        {categories instanceof Array &&
+          categories.map((category, i) => (
+            <Link key={i} to={"/" + category + "/products"}>{category}</Link>
+          ))
+        }
+      </div>
       <div className="products_container">
+        {loading &&
+          <>Loading data...</>
+        }
         {products instanceof Array &&
           products.map(product => (
             <Product {...product} key={product.id} />
