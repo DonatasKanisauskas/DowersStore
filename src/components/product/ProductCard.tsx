a105d8aaa64abbfac531735b09c38062991b5ada
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2024-02-28 13:56:08 +0200

    changed button to Button component

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import StarRating from "../StarRating";
 import Star from "../../assets/Star";
 import { productType } from "../../types/ProductType";
+import Button from "../Button";
 
 export default function ProductCard(product: productType) {
   const addToCart = (product: productType) => {
@@ -61,12 +62,7 @@ export default function ProductCard(product: productType) {
             </span>
 
             {/* Add To Cart */}
-            <button
-              onClick={() => addToCart(product)}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-              Add to cart
-            </button>
+            <Button onClick={() => addToCart(product)} value="Add to cart" />
           </div>
         </div>
       </div>
