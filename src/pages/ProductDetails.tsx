6930248dbac6b773499380eef942bdd23195f9c2
Donatas Kanisauskas <DonatasKanisauskas1@gmail.com>
2024-02-28 14:02:03 +0200

    Responsive and visual design enhancements.

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,15 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { productType } from "../types/ProductType";
-import React, { useEffect, useState } from "react";
-import logo from "/logo.svg";
+import { useEffect, useState } from "react";
 import StarRating from "../components/StarRating";
+import Carousel from "../components/Carousel";
+import Button from "../components/Button";
+import HeartOutline from "../assets/HeartOutline";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState<productType>();
-  const [selectedImage, setSelectedImage] = useState<string>();
+  const [selectedImage, setSelectedImage] = useState<number>(0);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  const addToCart = (product: productType) => {
+    console.log("adding", product.title, "to cart");
+  };
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -33,64 +39,55 @@ export default function ProductDetails() {
     fetchProduct();
   }, [id, navigate]);
 
-  const changeImage = (imageID: number) => {
-    setSelectedImage(product?.images[imageID]);
-  };
-
-  const zoomImage = (e: React.MouseEvent<HTMLElement>) => {
-    const image = e.currentTarget;
-    const rect = image.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / image.offsetWidth) * 100;
-    const y = ((e.clientY - rect.top) / image.offsetHeight) * 100;
-    image.style.backgroundPosition = x + "% " + y + "%";
-  };
-
   return (
     <>
       {loading && <>Loading data...</>}
       {id && product && (
-        <div>
-          <div>
-            <div>
-              <figure
-                onMouseMove={(e) => zoomImage(e)}
-                style={{
-                  backgroundImage: `url(${
-                    selectedImage || product.thumbnail || logo
-                  })`,
-                }}
+        <div className="flex flex-wrap 2xl:mx-40">
+          {/* LEFT / TOP */}
+          <Carousel
+            images={product.images}
+            selectedImage={selectedImage}
+            setSelectedImage={setSelectedImage}
+            className="w-full lg:w-7/12"
+          />
+
+          {/* RIGHT / BOTTOM */}
+          <div className="w-full flex flex-col justify-between lg:w-5/12 p-5 bg-gray-100">
+            {/* TOP */}
+            <div className="flex flex-col gap-1">
+              <div className="flex items-center flex-wrap justify-between my-5">
+                <h1 className="font-extrabold text-xl">{product.title}</h1>
+                <p className="pr-5">{product.brand}</p>
+              </div>
+              <p className="font-bold text-lg">${product.price}</p>
+              <p className="hidden">-{product.discountPercentage}%</p>
+              <p className="py-2">{product.description}</p>
+
+              <div className="flex gap-1">
+                <p className="font-bold">stock :</p>
+                <p>{product.stock}</p>
+              </div>
+
+              <div className="flex my-1">
+                <StarRating rating={product.rating} />
+                <span className="ml-2">({product.rating})</span>
+              </div>
+            </div>
+
+            {/* BOTTOM */}
+            <div className="flex gap-3 mt-10">
+              <Button
+                className="w-full"
+                onClick={() => addToCart(product)}
+                value="Add to cart"
+              />
+              <Button
+                className="h-full bg-transparent border border-gray-400 hover:bg-gray-200"
+                onClick={() => console.log("liked")}
               >
-                <img
-                  src={selectedImage || product.thumbnail || logo}
-                  alt="Logo"
-                />
-              </figure>
-            </div>
-            <div>
-              {product.images.map((image, i) => (
-                <img
-                  onClick={() => changeImage(i)}
-                  src={image}
-                  alt="img"
-                  key={i}
-                />
-              ))}
-            </div>
-          </div>
-          <div>
-            <p>{product.brand}</p>
-            <h3>{product.title}</h3>
-            <div>
-              <StarRating rating={product.rating} />
-              <span>({product.rating})</span>
-            </div>
-            <p>{product.description}</p>
-            <p>stock: {product.stock}</p>
-            <div className="m-t-auto">
-              <p>
-                ${product.price} <span>-{product.discountPercentage}%</span>
-              </p>
-              <button>add to cart</button>
+                <HeartOutline className=" text-black" />
+              </Button>
             </div>
           </div>
         </div>
